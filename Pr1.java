b0b93fe248e6559ac0ffae9a2d7ad8b21c17f830
rensitutorial <rensirafaliya@gmail.com>
2026-04-13 18:45:37 +0530

    Create Pr1.java

---
+++ Pr1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pr1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+        double feet = meters * 3.28084;
+        System.out.printf("Distance in feet: %.2f", feet);
+        sc.close();
+    }
+}
+
+
+
+output:Enter distance in meters: 10
+Distance in feet: 32.81
