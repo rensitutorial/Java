b548ccf7cc7c0debfad11b5bd905df0e3c45448b
rensitutorial <rensirafaliya@gmail.com>
2026-04-20 12:05:17 +0530

    Create Pr11.java

---
+++ Pr11.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class College {
+    String collegeName;
+
+    College(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    class Admission {
+        String studentName;
+        String course;
+
+        void acceptDetails() {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter Student Name: ");
+            studentName = sc.nextLine();
+            System.out.print("Enter Course: ");
+            course = sc.nextLine();
+        }
+
+        void displayDetails() {
+            System.out.println("\n--- Admission Details ---");
+            System.out.println("College Name : " + collegeName);
+            System.out.println("Student Name : " + studentName);
+            System.out.println("Course       : " + course);
+        }
+    }
+}
+
+public class Pr11 {
+    public static void main(String[] args) {
+        College college = new College("ABC Engineering College");
+        College.Admission admission = college.new Admission();
+        admission.acceptDetails();
+        admission.displayDetails();
+    }
+}
