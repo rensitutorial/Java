4fb81e1a93283a844be5e7d33435a3de6b23395d
rensitutorial <rensirafaliya@gmail.com>
2026-04-28 21:29:57 +0530

    Create Pr25.java

---
+++ Pr25.java
@@ -0,0 +1,51 @@
+
+
+class Table {
+    synchronized void printTable(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println();
+    }
+}
+
+class MyThread1 extends Thread {
+    Table t;
+
+    MyThread1(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(5);   
+    }
+}
+
+class MyThread2 extends Thread {
+    Table t;
+
+    MyThread2(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(7);   
+    }
+}
+
+public class Pr25 {
+    public static void main(String[] args) {
+        Table obj = new Table();
+
+        MyThread1 t1 = new MyThread1(obj);
+        MyThread2 t2 = new MyThread2(obj);
+
+        t1.start();
+        t2.start();
+    }
+}
